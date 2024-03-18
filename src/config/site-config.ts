39a77dd0a06072fd2d8a73b1b9385e7213be84ee
Faisal Tariq <faisaltariq183@gmail.com>
2024-03-18 21:52:50 +0400

    init(setup): next+shad

---
+++ src/config/site-config.ts
@@ -0,0 +1,9 @@
+import {Metadata} from 'next'
+
+const siteConfig: Metadata = {
+  title: 'Decentralized Exchange (DEX)',
+  description:
+    'Empower your trading experience with our decentralized exchange (DEX) app builder, facilitating seamless peer-to-peer transactions securely and efficiently.',
+}
+
+export default siteConfig
