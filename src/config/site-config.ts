1868006fb99e88133b0d04491d485ceed95f18cb
Faisal Tariq <faisaltariq183@gmail.com>
2024-03-19 00:15:30 +0400

    refactor(styles): shadcn

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -1,6 +1,4 @@
-import {Metadata} from 'next'
-
-const siteConfig: Metadata = {
+const siteConfig = {
   title: 'Decentralized Exchange (DEX)',
   description:
     'Empower your trading experience with our decentralized exchange (DEX) app builder, facilitating seamless peer-to-peer transactions securely and efficiently.',
