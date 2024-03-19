f4d322b98eb39afd23cc6ff13d7ead8dd2155e81
Faisal Tariq <faisaltariq183@gmail.com>
2024-03-19 14:52:00 +0400

    dep(setup): react-query

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,11 +2,14 @@
 import React, {ReactNode} from 'react'
 import {ThemeProvider} from './theme-provider'
 import {TooltipProvider} from '~/components/ui/tooltip'
+import ReactQueryProvider from './react-query'
 
 const Providers = ({children}: {children: ReactNode}) => {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      <TooltipProvider>{children}</TooltipProvider>
+      <ReactQueryProvider>
+        <TooltipProvider>{children}</TooltipProvider>
+      </ReactQueryProvider>
     </ThemeProvider>
   )
 }
